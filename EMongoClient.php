a744697f89c79b71c6f5ec9e70ef076d9550ab6f
Anton <koma136@mail.ru>
2016-04-28 15:37:06 +0300

    use mongodb + mongofill

--- EMongoClient.php
+++ EMongoClient.php
@@ -191,7 +191,7 @@ class EMongoClient extends CApplicationComponent
 	 */
 	public function connect()
 	{
-		if(!extension_loaded('mongo')){
+		if(!extension_loaded('mongodb')){
 			throw new EMongoException(
 				yii::t(
 					'yii', 
@@ -203,7 +203,7 @@ class EMongoClient extends CApplicationComponent
 		// We don't need to throw useless exceptions here, the MongoDB PHP Driver has its own checks and error reporting
 		// Yii will easily and effortlessly display the errors from the PHP driver, we should only catch its exceptions if
 		// we wanna add our own custom messages on top which we don't, the errors are quite self explanatory
-		if(version_compare(phpversion('mongo'), '1.3.0', '<')){
+		if(version_compare(phpversion('mongodb'), '1.1.0', '<')){
 			$this->_mongo = new Mongo($this->server, $this->options);
 			$this->_mongo->connect();
 			
